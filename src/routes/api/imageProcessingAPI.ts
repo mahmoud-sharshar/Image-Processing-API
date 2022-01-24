453a5e42e9837c9f94dd0ccef0bc053dec44cf3b
Mahmoud Hamed <mahmoud.hamed@blink22.com>
2022-01-24 07:22:31 +0200

    Create middleware for validating query parameters in resizing endpoint

--- src/routes/api/imageProcessingAPI.ts
+++ src/routes/api/imageProcessingAPI.ts
@@ -1,10 +1,14 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { isFileExists } from '../../utils/fileHelper';
 import { resizeImage } from '../../utils/imageProcessing';
 import { getAbsolutePath } from '../../utils/pathHelper';
 const router = express.Router();
 
-router.get('/resize', async (req, res) => {
+const validateQueryParams = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const width = Number(req.query.width);
   const height = Number(req.query.height);
   const fileName = req.query.filename;
@@ -15,11 +19,18 @@ router.get('/resize', async (req, res) => {
   const filePath = getAbsolutePath(`images/full/${fileName}`);
   if (!isFileExists(filePath))
     return res.status(400).send("Requested file doesn't exist");
+  next();
+};
+
+router.get('/resize', validateQueryParams, async (req, res) => {
+  const width = Number(req.query.width);
+  const height = Number(req.query.height);
+  const fileName = req.query.filename;
+  const filePath = getAbsolutePath(`images/full/${fileName}`);
   const resizedFilePath = getAbsolutePath(
     `images/thump/resized-${width}-${height}-${fileName}`
   );
   if (isFileExists(resizedFilePath)) return res.sendFile(resizedFilePath);
-
   try {
     await resizeImage(filePath, width, height, resizedFilePath);
     return res.sendFile(resizedFilePath);
